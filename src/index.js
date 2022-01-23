6994eed00c6bdb79aff3713ac347d19545d52205
Ciro Freitas <cirofreitas1995@gmail.com>
2022-01-23 19:28:56 -0300

    Fixed app.post("/repositories/:id/like"

--- src/index.js
+++ src/index.js
@@ -46,7 +46,6 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0)
@@ -62,13 +61,12 @@ app.post("/repositories/:id/like", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex < 0)
     return response.status(404).json({ error: "Repository not found" });
-  }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const likes = ++repositories[repositoryIndex].likes
 
-  return response.json('likes');
+  return response.status(201).json({ likes: likes });
 });
 
 module.exports = app;
